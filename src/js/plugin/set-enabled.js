7999e9ca024d45ff1fa685bc5589972fde21cdc8
Jesse Desaever <jessedesaever@web.de>
2018-10-02 11:44:58 +0200

    :lipstick: Cleanup set-enabled

--- src/js/plugin/set-enabled.js
+++ src/js/plugin/set-enabled.js
@@ -6,32 +6,31 @@ var cls = require('../lib/class')
 // Handlers
 var clickRailHandler = require('./handler/click-rail')
   , dragScrollbarHandler = require('./handler/drag-scrollbar')
-  , keyboardHandler = require('./handler/keyboard')
   , mouseWheelHandler = require('./handler/mouse-wheel')
   , nativeScrollHandler = require('./handler/native-scroll')
   , selectionHandler = require('./handler/selection')
   , touchHandler = require('./handler/touch');
 
 module.exports = function (element, enabled) {
-    var i = instances.get(element),
-        disabledCls = 'ps-disabled';
+  var i = instances.get(element),
+    disabledCls = 'ps-disabled';
 
-    if (enabled === true && i.enabled === false) {
-        clickRailHandler(element);
-        dragScrollbarHandler(element);
-        mouseWheelHandler(element);
-        nativeScrollHandler(element);
-        selectionHandler(element);
-        touchHandler(element);
+  if (enabled === true && i.enabled === false) {
+    clickRailHandler(element);
+    dragScrollbarHandler(element);
+    mouseWheelHandler(element);
+    nativeScrollHandler(element);
+    selectionHandler(element);
+    touchHandler(element);
 
-        cls.remove(i.scrollbarXRail, disabledCls);
-        cls.remove(i.scrollbarYRail, disabledCls);
-    } else if (enabled === false && (i.enabled === true || i.enabled === undefined)){
-        i.event.unbindAll();
-        cls.add(i.scrollbarXRail, disabledCls);
-        cls.add(i.scrollbarYRail, disabledCls);
-    }
+    cls.remove(i.scrollbarXRail, disabledCls);
+    cls.remove(i.scrollbarYRail, disabledCls);
+  } else if (enabled === false && (i.enabled === true || i.enabled === undefined)) {
+    i.event.unbindAll();
+    cls.add(i.scrollbarXRail, disabledCls);
+    cls.add(i.scrollbarYRail, disabledCls);
+  }
 
-    i.enabled = enabled;
+  i.enabled = enabled;
 
 };
